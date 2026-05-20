cabd0f8df7e7376602c8864cdc6c56322eaa6bf6
lucas-mansson <lu5132ma-s@student.lu.se>
2026-05-20 22:12:52 +0200

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<array<int, 26>, vector<string>> m;
+
+        for (auto const& s : strs) {
+            array<int, 26> arr{};
+            for (char c : s) {
+                arr[c - 'a']++;
+            }
+            cout << s << "\n";
+            for(int i = 0; i < arr.size(); i++) {
+                cout << arr[i];
+            }
+            cout << "\n";
+
+            auto it = m.find(arr);
+            if(it != m.end()) {
+                it->second.push_back(s);
+            } else {
+                m.insert({arr, {s}});
+            }
+        }
+        
+        vector<vector<string>> res;
+        for (auto& [key, value] : m) {
+            res.push_back(value);
+        }
+        return res;
+    }
+};
