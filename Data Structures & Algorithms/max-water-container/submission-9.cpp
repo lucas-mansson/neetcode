8384abc367a5d4cc29cfd3e30ab4917d560b3a06
lucas-mansson <lu5132ma-s@student.lu.se>
2026-07-06 13:36:53 +0200

    Add: max-water-container - submission-9

---
+++ Data Structures & Algorithms/max-water-container/submission-9.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int low(0);
+        int high(heights.size() - 1);
+        int maxArea(0);
+
+        while (low != high) {
+            int shortest = min(heights[low], heights[high]);
+            int area = (high - low) * shortest;
+            
+            maxArea = max(area, maxArea); 
+        
+            if (shortest == heights[low]) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+
+        return maxArea; 
+    }
+};
