e3ec42013a6fd0e6321683d218f956de7763eef5
lucas-mansson <lucasmansson02@gmail.com>
2026-08-18 09:59:29 +0200

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s.length() == 0 || s.length() == 1) {
+            return s.length();
+        }
+
+        int longest = 1;
+        int l = 0;
+        int r = 1;
+        unordered_set<char> chars{};
+        chars.insert(s[l]);
+
+        while (r < s.length()) {
+            if (chars.contains(s[r])) {
+                // duplicate at r = move l until no longer duplicate
+                while (chars.contains(s[r])) {
+                    chars.erase(s[l]);
+                    l++;
+                }
+            } 
+            int currLen = r - l + 1;
+            longest = max(currLen, longest);
+            chars.insert(s[r]);
+            r++;
+        }
+        return longest;
+    }
+};
